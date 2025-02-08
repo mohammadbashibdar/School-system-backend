43e654bd5638a884b52852f95844a488be3f05b4
Mohammad <bashibdar1384@gmail.com>
2025-02-08 22:39:11 +0330

    commited format_models.py

--- app/format_models.py
+++ app/format_models.py
@@ -14,15 +14,18 @@ class UserInDB(UserPublic):
     hashed_password: str
 
 
-class StudentPublic (BaseModel):
+class StudentPublic(BaseModel):
     name: str
+
 
 class StudentInDB(StudentPublic):
     hashed_password: str
 
+
 class EnrollmentPublic(BaseModel):
-    presenting_id : int
-    student_id : int
+    presenting_id: int
+    student_id: int
+
 
 class Token(BaseModel):
     access_token: str
@@ -34,23 +37,27 @@ class TokenData(BaseModel):
 
 
 class Log(BaseModel):
-    id : int
-    user_id : int
-    username : str | None
+    id: int
+    user_id: int
+    username: str | None
+
 
 class SurveyCategory(BaseModel):
     id: int
     section_name: str | None
 
+
 class Survey(BaseModel):
-    username: str |None
+    username: str | None
     title: str
     description: str
     category_id: int
 
+
 class FilterParams(BaseModel):
-    page_number: int = Field(1, gt= 0)
-    page_size: int = Field(5, gt=0, lt= 50)
+    id: int
+    page_number: int = Field(1, gt=0)
+    page_size: int = Field(5, gt=0, lt=50)
     order_by: str | None = None
     order_type: Literal["asc", "desc"] = "asc"
     query: str | None = None
