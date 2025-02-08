870e5ee5fe24fd65962c5643d55ef18509b27af9
Mohammad <bashibdar1384@gmail.com>
2025-02-08 22:25:33 +0330

    commited credit

--- app/routers/credit.py
+++ app/routers/credit.py
@@ -1,19 +1,20 @@
-from typing import Annotated, List
+from typing import Annotated, List, Optional
 from inspect import currentframe
-from fastapi import APIRouter, Depends, HTTPException
-from sqlmodel import Session, select
-from starlette import status
+from fastapi import APIRouter, Depends, HTTPException, status, Query
+from sqlmodel import Session, select, update, delete, or_
 from app.dependencies import get_session
 from app.db_models import Credit, User
 from app.routers.security import get_current_user, CREDENTIALS_EXCEPTION
+from app.format_models import FilterParams
 
 router = APIRouter()
+
 
 # Add Credit
 @router.post("/credits/add", tags=["credits"], response_model=Credit)
 async def add_credit(credit: Credit,
-                      current_user: Annotated[User, Depends(get_current_user)],
-                      session: Annotated[Session, Depends(get_session)]):
+                     current_user: Annotated[User, Depends(get_current_user)],
+                     session: Annotated[Session, Depends(get_session)]):
     try:
         if current_user.permission_group.name != 'full_permission_group':
             if currentframe().f_code.co_name not in current_user.permission_group.permissions:
@@ -29,15 +30,31 @@ async def add_credit(credit: Credit,
 
 # Read All Credits
 @router.get("/credits/", tags=["credits"], response_model=List[Credit])
-async def get_all_credits(current_user: Annotated[User, Depends(get_current_user)],
-                           session: Annotated[Session, Depends(get_session)]):
+async def get_all_credits(
+
+        current_user: Annotated[User, Depends(get_current_user)],
+        session: Annotated[Session, Depends(get_session)],
+        filter_query: Annotated[FilterParams, Query()]
+):
     if current_user.permission_group.name != 'full_permission_group':
         if currentframe().f_code.co_name not in current_user.permission_group.permissions:
             raise CREDENTIALS_EXCEPTION
-    credits = session.exec(select(Credit)).all()
-    if credits:
-        return credits
-    raise HTTPException(status_code=404, detail="Credits not found!")
+
+    query = select(Credit)
+
+    if filter_query.query:
+        query = query.where(or_(Credit.teacher_id.contains(filter_query.query),
+                                Credit.amount.contains(filter_query.query)))  # Filter by teacher_id or amount
+
+    if filter_query.order_by:
+        try:
+            sort_column = getattr(Credit, filter_query.order_by)  # Get the field from the model
+            query = query.order_by(sort_column.asc() if filter_query.order_type == "asc" else sort_column.desc())
+        except AttributeError:
+            raise HTTPException(status_code=400, detail=f"Invalid sort field: {filter_query.order_by}")
+
+    query = query.offset((filter_query.page_number - 1) * filter_query.page_size).limit(filter_query.page_size)
+    return session.exec(query).all()
 
 
 # Read Credit by ID
